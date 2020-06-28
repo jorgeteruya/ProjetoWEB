908a85281d2a88291a27d365d3f1ffceb10e06d0
jorgeteruya <40158063+jorgeteruya@users.noreply.github.com>
2020-06-28 01:30:43 -0300

    Busca estado e habilita spinner cidade

--- script/create-point.js
+++ script/create-point.js
@@ -18,7 +18,27 @@ populateUFs()
 
 function getCidade(event){
     const cidadeSelect = document.querySelector("select[name=cidade]")
-    console.log(event.target.value)
+    const ufInput = document.querySelector("input[name=estado]")
+
+    const ufValue = event.target.value
+
+    // const indexOfSelectedState = event.target.selectedIndex
+    // stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`
+
+    fetch(url)
+    .then(resposta => resposta.json())
+
+    .then( cidades =>{
+
+        for(const city of cidades){
+            cidadeSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        cidadeSelect.disabled = false   
+    })
+
 }
 
 document
